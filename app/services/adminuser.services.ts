9657ac64dd967e8ef479e1b0b74baf45f56a76cf
Angel <soporte@libus.com>
2023-05-23 14:36:18 -0300

    Se actualiza url base de backend

--- app/services/adminuser.services.ts
+++ app/services/adminuser.services.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { User } from "../types"
 
-const baseURL = "http://172.18.6.21:9005/"
+const baseURL = "https://172.18.0.167:11005/"
 
 export const axiosLogin = (url: string, data: User) =>{
     return axios.post(`${baseURL}${url}`, data, {
