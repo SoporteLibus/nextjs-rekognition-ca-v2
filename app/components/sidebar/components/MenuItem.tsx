37d28f4a0ac0975e9546b83acf05183433402e6c
Angel <soporte@libus.com>
2023-06-23 10:14:51 -0300

    Se arregla problema con carga de links

--- app/components/sidebar/components/MenuItem.tsx
+++ app/components/sidebar/components/MenuItem.tsx
@@ -9,7 +9,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ icon, text, link, setAuth }) => {
     <li>
       {
         text == "Salir" ?
-          <Link href={link} onClick={() => {
+          <Link href={`/${link}`} onClick={() => {
             deleteCookie("token")
             deleteCookie("user")
             setAuth && setAuth(false)
@@ -17,7 +17,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ icon, text, link, setAuth }) => {
           <span className={styles.icon}>{icon}</span>
           <span className={styles.title}>{text}</span>
         </Link> :
-        <Link href={link} > 
+        <Link href={`/${link}`} > 
           <span className={styles.icon}>{icon}</span>
           <span className={styles.title}>{text}</span>
         </Link>
